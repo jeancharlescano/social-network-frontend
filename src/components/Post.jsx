5382d6cf386be9a12e929e0e8859c71e06d6337b
jeancharlescano <jeancharles.cano@outlook.fr>
2023-04-24 00:21:06 +0200

    post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,19 +5,23 @@ import profile from "../assets/img/profile.jpeg";
 const Post = () => {
   return (
     <div className={styles.Container}>
-      <div className={styles.Header}>
-        <div className={styles.Profile}>
-          <div className={styles.ImgContainer}>
-            <img className={styles.Img} src={profile} />
-          </div>
-          <div className={styles.PostData}>
-            <p className={styles.Publisher}>Profile Name</p>
-            <p className={styles.Date}>12/04/2023 14h00</p>
+      <div className={styles.Post}>
+        <div className={styles.Header}>
+          <div className={styles.Profile}>
+            <div className={styles.ImgContainer}>
+              <img className={styles.Img} src={profile} />
+            </div>
+            <div className={styles.PostData}>
+              <p className={styles.Publisher}>Profile Name</p>
+              <p className={styles.Date}>12/04/2023 14h00</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.Content}>
-        
+        <div className={styles.Content}>
+          <p className={styles.Text}>
+          </p>
+        </div>
+        <div className={styles.ShowMore}>qsdqsdqsd</div>
       </div>
     </div>
   );
